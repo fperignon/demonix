a1c6ab735a773c222288e57b46fb395ad590db10
Franck Pérignon <franck.perignon@imag.fr>
2017-05-15 15:53:46 +0200

    ci

--- ci/install-demonix.sh
+++ ci/install-demonix.sh
@@ -3,6 +3,7 @@
 nix-env -i boost
 export BOOST_ROOT=$HOME/.nix-profile/include/
 export fftw_DIR=$HOME/.nix-profile/
-cmake -DWITH_BOOST=OFF -DBOOST_ROOT=/home/travis/.nix-profile/include/ -DWITH_FFTW=OFF -Dfftw_DIR=$HOME/.nix-profile $1 -DCMAKE_INSTALL_PREFIX=/home/travis/install/demonix
+ls $BOOST_ROOT
+cmake -DWITH_BOOST=ON -DBOOST_ROOT=/home/travis/.nix-profile/include/ -DWITH_FFTW=OFF -Dfftw_DIR=$HOME/.nix-profile $1 -DCMAKE_INSTALL_PREFIX=/home/travis/install/demonix
 make install
 
