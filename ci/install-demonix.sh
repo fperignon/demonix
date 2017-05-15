ab7e8cd27c7998eb1e8a64af7724a877a1e8c216
Franck Pérignon <franck.perignon@imag.fr>
2017-05-15 14:39:34 +0200

    ci

--- ci/install-demonix.sh
+++ ci/install-demonix.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-cmake -DWITH_BOOST=ON -DBOOST_ROOT=~/.nix-profile/include -DWITH_FFTW=ON -Dfftw_DIR=~/.nix-profile $1
+ls $HOME/.nix-profile
+cmake -DWITH_BOOST=ON -DBOOST_ROOT=$HOME/.nix-profile/include -DWITH_FFTW=ON -Dfftw_DIR=$HOME/.nix-profile $1
 make install
 
